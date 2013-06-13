e4e26b28e98f38678b886ba02cb3481e749b913a
theburningmonk <theburningmonk@gmail.com>
2013-06-13 02:44:32 +0100

    - added assembly info - added license files

---
+++ src/FSharp.Markdown.Pdf/AssemblyInfo.fs
@@ -0,0 +1,17 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: ComVisible(false)>]
+
+[<assembly: AssemblyTitle("FSharp.Markdown.Pdf")>]
+[<assembly: AssemblyDescription("Formats Markdown documents to PDF")>]
+
+[<assembly: AssemblyProduct("FSharp.Markdown.Pdf")>]
+[<assembly: AssemblyCopyright("Copyright @ Yan Cui 2013")>]
+[<assembly: AssemblyVersion("0.1.0")>]
+[<assembly: AssemblyFileVersion("0.1.0")>]
+
+do ()
