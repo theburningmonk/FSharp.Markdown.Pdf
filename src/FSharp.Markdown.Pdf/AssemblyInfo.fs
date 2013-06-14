0bd46d8c2e2e46bfdc3455a0817d1137ee4ea6f2
theburningmonk <theburningmonk@gmail.com>
2013-06-14 02:14:58 +0100

    - bumped version number

--- src/FSharp.Markdown.Pdf/AssemblyInfo.fs
+++ src/FSharp.Markdown.Pdf/AssemblyInfo.fs
@@ -11,7 +11,7 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyProduct("FSharp.Markdown.Pdf")>]
 [<assembly: AssemblyCopyright("Copyright @ Yan Cui 2013")>]
-[<assembly: AssemblyVersion("0.1.0")>]
-[<assembly: AssemblyFileVersion("0.1.0")>]
+[<assembly: AssemblyVersion("0.2.0")>]
+[<assembly: AssemblyFileVersion("0.2.0")>]
 
 do ()
